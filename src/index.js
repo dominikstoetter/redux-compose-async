8589bb92905292d28b75fc11d3a21ec9b9f1e631
Dominik Stötter <dominik.stoetter@yahoo.com>
2019-07-18 19:46:11 +0200

    add utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const actions = require('../lib/actions')
 const reducer = require('../lib/reducer')
 const sagas = require('../lib/sagas')
+const isDefined = require('../lib/is-defined')
+const isUndefined = require('../lib/is-undefined')
 
 module.exports = {
   ...actions,
   ...reducer,
   ...sagas,
+  utils: {
+    isDefined,
+    isUndefined
+  }
 }
