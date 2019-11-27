884d9a4db408e95f983f011f03de6f82250d204f
Dominik Stötter <dominik.stoetter@8select.de>
2019-11-27 15:27:16 +0100

    fix named export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const actions = require('../lib/actions')
 const reducer = require('../lib/reducer')
 const sagas = require('../lib/sagas')
-const store = require('../lib/provide-store')
 const isDefined = require('../lib/is-defined')
 const isUndefined = require('../lib/is-undefined')
 const queryString = require('../lib/query-string')
@@ -10,7 +9,6 @@ module.exports = {
   ...actions,
   ...reducer,
   ...sagas,
-  ...store,
   utils: {
     isDefined,
     isUndefined,
