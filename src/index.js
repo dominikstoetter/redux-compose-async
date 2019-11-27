489e381c3f1a728dd499be59795ba3029454e17f
Dominik Stötter <dominik.stoetter@8select.de>
2019-11-27 10:47:07 +0100

    1.0.0

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const actions = require('../lib/actions')
 const reducer = require('../lib/reducer')
 const sagas = require('../lib/sagas')
+const store = require('../lib/provide-store')
 const isDefined = require('../lib/is-defined')
 const isUndefined = require('../lib/is-undefined')
+const queryString = require('../lib/query-string')
 
 module.exports = {
   ...actions,
   ...reducer,
   ...sagas,
+  ...store,
   utils: {
     isDefined,
-    isUndefined
-  }
+    isUndefined,
+    ...queryString,
+  },
 }
